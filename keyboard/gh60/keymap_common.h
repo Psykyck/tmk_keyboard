11ac9e054acdfbb91640b382d3a6e0a90639e098
Clement <clementlu94@gmail.com>
2015-12-19 18:57:32 -0500

    fix errors

--- keyboard/gh60/keymap_common.h
+++ keyboard/gh60/keymap_common.h
@@ -42,7 +42,7 @@ extern const uint16_t fn_actions[];
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      K2E, \
     K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, \
-    K40, K41, K42,                K46,           K49, K4A, K4B, K4C, K4D, K4E, \
+    K40, K41, K42,                K46,           K49, K4A, K4B, K4C, K4D, K4E \
 ) { \
     { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E }, \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E }, \
